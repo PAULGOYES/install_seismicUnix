882f346f3e4d55e3f57d904dd61af3f72b32c4f0
PAULGOYES <goyes.yesid@gmail.com>
2017-06-20 03:06:54 +0500

    Add files via upload

---
+++ su.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+
+# Para su uso copia este script en la carpeta donde esta el comprimido
+# del fuente de Seismic Unix. Gracias por usar este script.
+
+
+
+# Elaborado por :e-mail: silva.l.a.l@gmail.com (A. L. Silva).
+
+# Modificado por: goyes.yesid@gmail.com /oct/2016
+
+
+
+################# ----- ANTES DE INSTALAR EL PROGRAMA VERIFICAR QUE TENGA INSTALADO COMPILADORES C++ C FORTRAN #################
+
+# dnf groupinstall "Development tools" para Fedora.
+
+
+
+# Puede descargar el Archivo de instalacion directamente desde la pagina web del CWP MINES
+# http://www.cwp.mines.edu/cwpcodes/
+
+# o simplemente copiando esto en su terminal: wget ftp://ftp.cwp.mines.edu/pub/cwpcodes/cwp_su_all_44R5.tgz
+
+
+export CWPROOT=$HOME/SU/
+
+export PATH=$PATH:$HOME/SU/bin/
+
+# Creando directorio de trabajo...
+
+echo "Creando directorio de trabajo..."
+
+if ls $HOME/SU/
+
+then
+
+    echo "OK"
+
+else
+
+    echo "Creando directorio $HOME/SU"
+
+    mkdir $HOME/SU/
+
+fi
+
+# configuraciones del archivo bashrc...
+
+echo "Configurando el archivo .bashrc..."
+
+if cat $HOME/.bashrc | grep CWPROOT
+
+then echo "OK"
+
+else echo "export CWPROOT=$HOME/SU" >> $HOME/.bashrc
+
+     echo "export PATH=$PATH:$HOME/SU/bin" >> $HOME/.bashrc
+
+fi
+
+
+
+if cat /etc/issue | grep -i Ubuntu
+
+then sudo apt-get install libxmu-dev libgl1-mesa-dev libglw1-mesa-dev libglu1-mesa-dev glutg3-dev libxt-dev libxrender-dev libxrandr-dev libxi-dev libxinerama-dev libx11-dev
+
+elif cat /etc/issue | grep -i Fedora
+
+then su -c 'dnf -y install libXmu-devel mesa-libGL-devel mesa-libGLU-devel mesa-libGLw-devel freeglut-devel libXt-devel libXrender-devel libXrandr-devel libXi-devel libXinerama-devel libX11-devel'
+
+else echo "other Linux OS (not configured here). Good luck!"
+
+fi
+
+
+
+echo "Copiando fuentes..."
+
+cp cwp_su_all* $CWPROOT/
+
+cd $CWPROOT && tar -zxvf *.tgz
+
+echo "Entrando en directorio de trabajo y compilando..."
+
+cd $CWPROOT/src && make install
+
+echo "Set basico de codigos de SU instalado..."
+
+cd $CWPROOT/src && make xtinstall
+
+echo "Aplicaciones X-toolkit instaladas..."
+
+cd $CWPROOT/src && make mglinstall
+
+echo "Mesa/Open GL items instalados..."
+
+cd $CWPROOT/src && make utils
+
+echo "Libcwputils instalados..."
+
+cd $CWPROOT/src && make xminstall
+
+echo "Aplicaciones Motif instaladas..."
+
+cd $CWPROOT/src && make sfinstall
+
+
+
+source ~/.bashrc
+
+clear
+
+exit 0
+
+source ~/.bashrc
+
+echo "All done!!, try.....$ suplane | suxwigb &  "
